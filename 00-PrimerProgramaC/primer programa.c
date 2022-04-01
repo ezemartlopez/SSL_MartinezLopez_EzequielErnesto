4e387b9280400f4eeac1ad8ec3140b496dd5d258
ezemartlopez <zenitram.e14@gmail.com>
2022-04-01 12:40:45 -0300

    modificacion del programa y se agrega el ejecutable

--- 00-PrimerProgramaC/primer programa.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(){
-printf("Hola mundo, miprimer programa en c.");
-return 0;
-}
---
+++ 00-PrimerProgramaC/primerPrograma.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+printf("Hola mundo, mi primer programa en c.");
+return 0;
+}
